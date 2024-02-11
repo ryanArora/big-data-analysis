0e6a265944aaae251b947af424e6bb21aa8e941a
Ryan Arora <arora.ryan@gmail.com>
2024-02-10 19:19:17 -0500

    seed

--- seed.ts
+++ seed.ts
@@ -1,1 +1,17 @@
-console.log("Hi");
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
+
+async function main() {
+  console.log("seed");
+}
+
+main()
+  .then(async () => {
+    await db.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await db.$disconnect();
+    process.exit(1);
+  });
